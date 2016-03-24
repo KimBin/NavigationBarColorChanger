e1c7c7e5cec3eff98a63732dfc49e0fc38a79358
xiejun <273794982@qq.com>
2016-03-24 10:32:09 +0800

    Initial Commit

---
+++ NavigationBarGradient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NavigationBarGradient
+//
+//  Created by 看楼听雨 on 16/3/24.
+//  Copyright © 2016年 XJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NavigationBarGradient/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NavigationBarGradient
+//
+//  Created by 看楼听雨 on 16/3/24.
+//  Copyright © 2016年 XJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
